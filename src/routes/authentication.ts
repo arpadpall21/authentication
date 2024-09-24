bfd464d9c3c5047f5d8486426e9b8351e61c5053
Arpad Pall <a.pall21@yahoo.fr>
2024-09-24 14:50:54 +0200

    respons format + passport validator

--- src/routes/authentication.ts
+++ src/routes/authentication.ts
@@ -1,25 +1,36 @@
 import { Router, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
+import passwordValidator from 'password-validator';
 
 interface LoginOrRegisterBody {
   user: string;
   password: string;
 }
 
+interface AuthResponse {
+  success: boolean;
+  message?: string;
+}
+
 const authRouter = Router();
 
-authRouter.post('/register', (req: Request<object, object, LoginOrRegisterBody>, res: Response) => {
+authRouter.post('/register', (req: Request<object, object, LoginOrRegisterBody>, res: Response<AuthResponse>) => {
   console.log(req.body)
 
-  res.send('REGISTER OK');
+  
+  
+  res.send({ success: true });
 });
 
-authRouter.post('/login', (req: Request<object, object, LoginOrRegisterBody>, res: Response) => {
-  res.send('LOGIN OK');
+authRouter.post('/login', (req: Request<object, object, LoginOrRegisterBody>, res: Response<AuthResponse>) => {
+
+  res.send({ success: true });
 });
 
-authRouter.get('/logout', (req: Request, res: Response) => {
-  res.send('LOGOUT OK');
+authRouter.get('/logout', (req: Request, res: Response<AuthResponse>) => {
+
+
+  res.send({ success: true });
 });
 
 export default authRouter;
